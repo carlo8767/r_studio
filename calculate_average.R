33137c81f0c6e64fb8a61e1da24a7fd1401dc3b2
carraroc <c.carraro@lancaster.ac.uk>
2024-05-06 15:50:23 +0200

    firstExercise

---
+++ calculate_average.R
@@ -0,0 +1,10 @@
+num1 <-0
+num2 <- 1
+average= num1+num2/2
+print(average)
+my_function <- function (numOne,numTwo){
+  calculation=numOne+numTwo
+  final = calculation/2
+  concatenation = paste("The answer is ", final)
+  print(concatenation)
+}
