4e58b9b12b771a29805bd9bcf8d75b39129abc27
carraroc <c.carraro@lancaster.ac.uk>
2024-05-07 15:42:22 +0200

    concatenate_vector

--- variousExercise/analyze_student_score.R
+++ variousExercise/analyze_student_score.R
@@ -5,7 +5,11 @@
     english_scores =c(60,71,58,69),
     ages = c(19,23,34,45)
   )
-
+# use the function rbing to concatenate three different vector
+ts = rbind(student_data$math_scores, student_data$science_scores)
+summary.at
+# get the median
+print(median(ts))
 average_age  <- median(student_data$ages)
 print(paste("The average age is", average_age))
 
